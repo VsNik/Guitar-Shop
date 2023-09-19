990ed9847396c6451762ca4b0832fc932e7aa271
VsNik <V10Nik80@mail.ru>
2023-09-19 11:36:21 +0300

    cli

--- cli/src/app/app.module.ts
+++ cli/src/app/app.module.ts
@@ -8,13 +8,15 @@ import {Product, User} from "@guitar-shop/lib/models";
 import {ProductModule} from "./products/product.module";
 import {UsersModule} from "./uders/users.module";
 
+const DB_URL= 'postgresql://admin:secret@localhost:5432/guitar-shop';
+
 @Module({
   imports: [
     ProductModule,
     UsersModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: 'postgresql://admin:secret@localhost:5432/guitar-shop',
+      url: DB_URL,
       entities: [Product, User],
     })
   ],
